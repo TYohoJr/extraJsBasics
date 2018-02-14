fdea9dc923e450c648a80fbafd29ebac2fb86c19
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-14 09:37:05 -0700

    worked on bcu

--- bcu.js
+++ bcu.js
@@ -1,1 +1,12 @@
 //Given a string, write a function that returns the number of time the letter "b", "c" or "u" appear in that string.
+function strTest(myStr) {
+    var counter = 0;
+    for(var i = 0; i < myStr.length; i++) {
+        if(myStr[i] == "b" || myStr[i] == "c" || myStr[i] == "u") {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+console.log(strTest("qweqbbbuceq"))
