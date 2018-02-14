5debefda2a02632ad81b781b0b1f5ce44659ea6d
kramduckner <buckner.a.mark@gmail.com>
2018-02-13 20:16:14 -0700

    Create method.js

---
+++ method.js
@@ -0,0 +1,11 @@
+var plant = {
+  kingdom:"plantae",
+  genus:"Epipremnum",
+  commonName:"dumb house plant"
+  fetchName: function(){
+    //write code in here
+  }
+}
+
+//Fill in the fetchName function to grab the properties kingdom and genus, and returns the string: "the {commonName}, of kingdom {kingdom} and genus {genus}, is next to the closet behind us."
+//write a test for it using mocha and chai
