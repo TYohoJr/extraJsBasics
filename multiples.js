978df0562ab1abcee6e78a8ea18e5b6d2daf7c6c
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-14 14:41:49 -0700

    worked on multples and reverse

--- multiples.js
+++ multiples.js
@@ -1,1 +1,13 @@
 //Write a JavaScript program check if a given positive number is a multiple of 3 or a multiple of 7. The function should return true or false (a boolean).
+
+var testNum = 21;
+
+function threeSevenTest(num) {
+    var result = false;
+    if((num > 0) && (num % 3 == 0) && (num % 7 == 0)){
+        result = true;
+    }
+    return result;
+}
+
+console.log(threeSevenTest(testNum));
--- reverse.js
+++ reverse.js
@@ -1,1 +1,13 @@
 //write a function to reverse a given string.
+
+var testString = "reverse me"
+
+function reverseStr(string){
+    var newString = "";
+    for(var i = string.length; i >= 0; i--){
+        newString += string.charAt(i);
+    }
+    return newString;
+}
+
+console.log(reverseStr(testString));
