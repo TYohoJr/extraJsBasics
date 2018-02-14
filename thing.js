8d554bd852d6bc56ca10ef353ba0c71f86f50157
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-14 14:53:30 -0700

    started thing.js

--- thing.js
+++ thing.js
@@ -12,6 +12,19 @@
     }
   }
 
+  function menuReturn(testObj){
+    for(var i =0; i < testObj.length; i++){
+      for(var j = 0; j < testObj[i].length; j++){
+        for(var k = 0; k < testObj[i][j].length; k++){
+          if(testObj[i][j][k][key] == menuitem)
+          return testObj[i][j][k];
+        }
+      }
+    }
+    console.log(testObj[0][3][0]);
+  }
+
+  console.log(menuReturn(thing));
 //write a function that iterates over the menuItems array in the object "thing" and returns the value property lower-cased
 
 //write a function that accepts an object, and if the menu.id in the object is null, it returns "we need an id for this object,"
